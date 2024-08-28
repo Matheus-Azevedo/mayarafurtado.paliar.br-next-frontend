fabd4023c6a713e398e19e4531e05e0da3881902
Matheus Eduardo <matheuseduardo.jp@gmail.com>
2024-08-28 15:04:44 -0300

    pagina da biografia

---
+++ src/app/sections/biography/biography.tsx
@@ -0,0 +1,30 @@
+import "./biography.css";
+
+function Biography() {
+  return (
+    <main className="biography-container-1">
+      <article className="biography-content">
+        <h1>Minha história...</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod
+          tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim
+          veniam, quis nostrum exercitationem ullam corporis suscipit
+          laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis aute iure
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum
+          dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt
+          ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+          nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut
+          aliquid ex ea commodi consequatur. Quis aute iure reprehenderit in
+          voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+          sint obcaecat cupiditat non proident, sunt in culpa qui officia
+          deserunt mollit anim id est laborum.
+        </p>
+      </article>
+      <div className="biography-photo"></div>
+    </main>
+  );
+}
+
+export default Biography;
