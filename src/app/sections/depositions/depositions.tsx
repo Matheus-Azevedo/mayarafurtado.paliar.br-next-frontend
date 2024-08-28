10d3457d86d99947780e0a11f7a6a3e87423434e
Matheus Eduardo <matheuseduardo.jp@gmail.com>
2024-08-28 15:04:23 -0300

    pagina de depoimentos

---
+++ src/app/sections/depositions/depositions.tsx
@@ -0,0 +1,30 @@
+import "./depositions.css";
+
+function Depositions() {
+  return (
+    <main className="depositions-container-1">
+      <div className="depositions-photo"></div>
+      <article className="depositions-content">
+        <h1>Meu depoimento...</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod
+          tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim
+          veniam, quis nostrum exercitationem ullam corporis suscipit
+          laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis aute iure
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum
+          dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt
+          ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+          nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut
+          aliquid ex ea commodi consequatur. Quis aute iure reprehenderit in
+          voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+          sint obcaecat cupiditat non proident, sunt in culpa qui officia
+          deserunt mollit anim id est laborum.
+        </p>
+      </article>
+    </main>
+  );
+}
+
+export default Depositions;
