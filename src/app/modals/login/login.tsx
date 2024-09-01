f561b0cb6f14e59c690644f096975669f4b5c8fe
Matheus Eduardo <matheuseduardo.jp@gmail.com>
2024-09-01 17:32:19 -0300

    login component

---
+++ src/app/modals/login/login.tsx
@@ -0,0 +1,74 @@
+"use client";
+
+import React, { useState } from "react";
+import Image from "next/image";
+import "./login.css";
+import { sendLoginRequest } from "@/services/login";
+import Lobby from "../lobby/lobby";
+
+interface LoginProps {
+  closeLoginModal: () => void;
+}
+
+function Login({ closeLoginModal }: LoginProps) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [openLobbyModal, setOpenLobbyModal] = useState(false);
+
+  function handleChangeEmail(event: React.ChangeEvent<HTMLInputElement>) {
+    setEmail(event.target.value);
+  }
+
+  function handleChangePassword(event: React.ChangeEvent<HTMLInputElement>) {
+    setPassword(event.target.value);
+  }
+
+  async function handleLogin() {
+    const data = await sendLoginRequest(email, password);
+    if (data) {
+      console.log(data);
+      setOpenLobbyModal(true);
+    }
+    // console.log(email, password);
+    // console.log("Login successful");
+    // setOpenLobbyModal(true);
+    // closeLoginModal();
+  }
+
+  function closeLobbyModal() {
+    setOpenLobbyModal(false);
+  }
+
+  return (
+    <>
+      <dialog className="login-modal">
+        <div className="login-image">
+          <Image src="/logo2.png" alt="logo" width={100} height={100} />
+        </div>
+        <input
+          className="login-input"
+          type="text"
+          value={email}
+          onChange={handleChangeEmail}
+          placeholder="Email"
+        />
+        <input
+          className="login-input"
+          type="password"
+          value={password}
+          onChange={handleChangePassword}
+          placeholder="Password"
+        />
+        <button className="login-button" onClick={handleLogin}>
+          Login
+        </button>
+        <button className="close-button" onClick={closeLoginModal}>
+          Close
+        </button>
+      </dialog>
+      {openLobbyModal && <Lobby closeLobbyModal={closeLobbyModal} />}
+    </>
+  );
+}
+
+export default Login;
