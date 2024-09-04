fb67940778ab57b4849be8e0d3c3febbfbf41f5c
Matheus Eduardo <matheuseduardo.jp@gmail.com>
2024-09-04 09:20:42 -0300

    landing

---
+++ src/app/modals/lobbyLanding/lobbyLanding.tsx
@@ -0,0 +1,11 @@
+import "./lobbyLanding.css";
+
+function LobbyLanding() {
+  return (
+    <section className="lobby-landing-container">
+      <h1>Lobby Landing</h1>
+    </section>
+  );
+}
+
+export default LobbyLanding;
