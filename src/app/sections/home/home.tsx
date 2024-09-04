0b87381d47fb67a28f883cf681c5b850f276d65c
Matheus Eduardo <matheuseduardo.jp@gmail.com>
2024-09-04 09:19:30 -0300

    home update

--- src/app/sections/home/home.tsx
+++ src/app/sections/home/home.tsx
@@ -1,51 +1,69 @@
+"use client";
+
 import Link from "next/link";
 import "./home.css";
+import Login from "../../modals/login/login";
+import { useState } from "react";
 
 function Home() {
+  const [openLoginModal, setLoginModal] = useState(false);
+
+  function closeLoginModal() {
+    setLoginModal(false);
+  }
+
   return (
-    <main className="home-container-1">
-      <div className="home-container-2">
-        <div className="identity">
-          <h1>Mayara Furtado.</h1>
-          <p>
-            GERONTOLOGIA E PALIATIVISMO
-            <br />
-            CREFITO: 217440-F
-          </p>
+    <>
+      <main className="home-container-1">
+        <div className="home-container-2">
+          <div className="identity">
+            <button onClick={() => setLoginModal(true)}>
+              <h1>Mayara Furtado.</h1>
+            </button>
+            <p>
+              GERONTOLOGIA E PALIATIVISMO
+              <br />
+              CREFITO: 217440-F
+            </p>
+          </div>
+          <nav>
+            <Link href="#biography" scroll={true}>
+              <button>Biografia</button>
+            </Link>
+            <Link href="#clinic" scroll={true}>
+              <button>Clínica</button>
+            </Link>
+            <Link href="#depositions" scroll={true}>
+              <button>Depoimentos</button>
+            </Link>
+            <Link href="#contacts" scroll={true}>
+              <button>Contato</button>
+            </Link>
+          </nav>
         </div>
-        <nav>
-          <Link href="#biography" scroll={true}>
-            <button>Biografia</button>
-          </Link>
-          <Link href="#clinic" scroll={true}>
-            <button>Clínica</button>
-          </Link>
-          <Link href="#depositions" scroll={true}>
-            <button>Depoimentos</button>
-          </Link>
-          <Link href="#contacts" scroll={true}>
-            <button>Contato</button>
-          </Link>
-        </nav>
-      </div>
-      <div className="home-container-3">
-        <div className="home-photo"></div>
-        <div className="home-content">
-          <h1>
-            Me chamo
-            <strong className="brown-paliar"> Mayara Furtado,</strong>
-            <br />
-            Fisioterapeuta{" "}
-            <strong className="brown-paliar">Gerontóloga,</strong>
-            <br />
-            com foco em
-            <strong className="brown-paliar"> reabilitação funcional</strong>
-            <br />e <strong className="brown-paliar">doenças crônicas.</strong>
-          </h1>
-          <p>Proporcionando cuidado integral ao idoso com dedicação e amor.</p>
+        <div className="home-container-3">
+          <div className="home-photo"></div>
+          <div className="home-content">
+            <h1>
+              Me chamo
+              <strong className="brown-paliar"> Mayara Furtado,</strong>
+              <br />
+              Fisioterapeuta{" "}
+              <strong className="brown-paliar">Gerontóloga,</strong>
+              <br />
+              com foco em
+              <strong className="brown-paliar"> reabilitação funcional</strong>
+              <br />e{" "}
+              <strong className="brown-paliar">doenças crônicas.</strong>
+            </h1>
+            <p>
+              Proporcionando cuidado integral ao idoso com dedicação e amor.
+            </p>
+          </div>
         </div>
-      </div>
-    </main>
+      </main>
+      {openLoginModal && <Login closeLoginModal={closeLoginModal} />}
+    </>
   );
 }
 
